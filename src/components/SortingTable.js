224ad79bad2ef829d7bf85da3b0e4e6da2c80d73
StjepanDev <grunf69@yahoo.com>
2021-11-14 13:18:17 +0100

    filtering table

--- src/components/SortingTable.js
+++ src/components/SortingTable.js
@@ -1,17 +1,20 @@
 import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
+import { useTable, useSortBy } from 'react-table';
 import { COLUMNS } from './columns';
 import MOCK_DATA from './MOCK_DATA.json';
 import './table.css';
 
-export const BasicTable = () => {
+export const SortingTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
 
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
 
   const {
     getTableProps,
@@ -28,7 +31,13 @@ export const BasicTable = () => {
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}> {column.render('Header')}</th>
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {' '}
+                {column.render('Header')}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? '▼' : '▲') : ''}
+                </span>
+              </th>
             ))}
           </tr>
         ))}
